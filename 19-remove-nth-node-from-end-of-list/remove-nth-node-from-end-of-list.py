182ee259c236d10249add52d591379e0e7b89737
riddhimarai37 <57564654+riddhimarai37@users.noreply.github.com>
2023-10-04 01:02:32 -0400

    Time: 38 ms (74.03%) | Memory: 16.3 MB (22.71%) - LeetSync

--- 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.py
+++ 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.py
@@ -5,30 +5,53 @@
 #         self.next = next
 class Solution:
     def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
-        length = 0
+        # CODEPATH SOLUTION
 
-        curr = head
-        while curr:
-            length += 1
-            curr = curr.next
+        temp = ListNode(val = 0, next = head)
 
-        remove_idx = length - n
-        if length == 1:
-            return head.next
-        elif remove_idx == 0:
-            return head.next
+        first = temp
+        second = temp
 
-        prev = curr = head
+        # Advances first pointer so that the gap between first and second is n nodes apart
+        for i in range (n+1):
+            first = first.next
 
-        curr_idx = 0
-        while curr:
-            if curr_idx == remove_idx:
-                prev.next = curr.next
-                curr = curr.next
-            else:
-                prev = curr
-                curr = curr.next
-            curr_idx += 1
+        # While the first pointer does not equal null move both first and second to maintain the gap and get nth node from the end
+        while (first != None):
+            first = first.next 
+            second = second.next
 
-            
-        return head
+        # Delte the node being pointed to by second
+        second.next = second.next.next
+
+        return temp.next
+
+
+
+        # MY SOLUTION 
+        # length = 0
+
+        # curr = head
+        # while curr:
+        #     length += 1
+        #     curr = curr.next
+
+        # remove_idx = length - n
+        # if length == 1:
+        #     return head.next
+        # elif remove_idx == 0:
+        #     return head.next
+
+        # prev = curr = head
+
+        # curr_idx = 0
+        # while curr:
+        #     if curr_idx == remove_idx:
+        #         prev.next = curr.next
+        #         curr = curr.next
+        #     else:
+        #         prev = curr
+        #         curr = curr.next
+        #     curr_idx += 1
+
+        # return head
