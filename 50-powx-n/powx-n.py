af83f942da8f3b423b1aea974ae747ee45fbb8cc
riddhimarai37 <57564654+riddhimarai37@users.noreply.github.com>
2024-01-08 16:47:18 -0500

    Time: 30 ms (94.43%) | Memory: 17.6 MB (13.97%) - LeetSync

--- 50-powx-n/powx-n.py
+++ 50-powx-n/powx-n.py
@@ -1,15 +1,27 @@
 class Solution:
     def myPow(self, x: float, n: int) -> float:
-        if n == 0:
-            return 1
-        elif n == 1:
-            return x
-        elif n == -1:
-            return 1/x
+        # if n == 0:
+        #     return 1
+        # elif n == 1:
+        #     return x
+        # elif n == -1:
+        #     return 1/x
             
-        result = self.myPow(x,n//2)
-        return result * result * self.myPow(x,n%2)
+        # result = self.myPow(x,n//2)
+        # return result * result * self.myPow(x,n%2)
 
+
+        def function(base=x, exponent=abs(n)):
+            if exponent == 0:
+                return 1
+            elif exponent % 2 == 0:
+                return function(base * base, exponent // 2)
+            else:
+                return base * function(base * base, (exponent - 1) // 2)
+
+        f = function()
+        
+        return float(f) if n >= 0 else 1/f
 
 
 
