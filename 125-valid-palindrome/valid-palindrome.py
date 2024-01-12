248478342090a3f9e60458a20cc830ec2a50c368
riddhimarai37 <57564654+riddhimarai37@users.noreply.github.com>
2024-01-12 16:16:53 -0500

    Time: 39 ms (93.76%) | Memory: 17.8 MB (41.94%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -21,3 +21,13 @@ class Solution:
             right -=1
 
         return True
+
+# neetcode solution 
+
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        new = ''
+        for a in s:
+            if a.isalpha() or a.isdigit():
+                new += a.lower()
+        return (new == new[::-1])
