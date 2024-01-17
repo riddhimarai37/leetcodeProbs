48fd0e971b16e52f145f7236a44854346b7c2f40
riddhimarai37 <57564654+riddhimarai37@users.noreply.github.com>
2024-01-17 13:35:05 -0500

    Time: 34 ms (89.13%) | Memory: 18.6 MB (35.13%) - LeetSync

--- 206-reverse-linked-list/reverse-linked-list.py
+++ 206-reverse-linked-list/reverse-linked-list.py
@@ -5,22 +5,65 @@
 #         self.next = next
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        # Initialize a previous node, a current node.
-        prev, curr = None, head
+        if head == None or head.next == None:
+            return head
 
-        # While current node is not null
+        prev = None
+        curr = head
+
         while curr:
-            # Initialize a next node to temporarily hold the next node
+            # next value to look at
             next = curr.next
-            # Point current node .next to previous node
-            curr.next = prev
-            # Set prev node to current node
+
+            # reversing 
+            curr.next = prev 
             prev = curr
-            # Set current node to next node
             curr = next
-        
-        # Return previous node
         return prev
+        
+             
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        # Initialize a previous node, a current node.
+        # prev, curr = None, head
+
+        # # While current node is not null
+        # while curr:
+        #     # Initialize a next node to temporarily hold the next node
+        #     next = curr.next
+        #     # Point current node .next to previous node
+        #     curr.next = prev
+        #     # Set prev node to current node
+        #     prev = curr
+        #     # Set current node to next node
+        #     curr = next
+        
+        # # Return previous node
+        # return prev
         # if not head or not head.next:
         #     return head
 
