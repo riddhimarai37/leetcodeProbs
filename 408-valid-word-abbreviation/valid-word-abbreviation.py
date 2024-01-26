ee00d4dcb892a554edd4243c883842504b98d2a0
riddhimarai37 <57564654+riddhimarai37@users.noreply.github.com>
2024-01-26 12:00:34 -0500

    Time: 38 ms (57.10%) | Memory: 16.5 MB (61.13%) - LeetSync

--- 408-valid-word-abbreviation/valid-word-abbreviation.py
+++ 408-valid-word-abbreviation/valid-word-abbreviation.py
@@ -1,23 +1,66 @@
 class Solution:
     def validWordAbbreviation(self, word: str, abbr: str) -> bool:
-        str_pt = 0
-        abbr_pt = 0
+        abbr_pt = word_pt = 0
 
-        while str_pt < len(word) and abbr_pt < len(abbr):
+        while word_pt < len(word) and abbr_pt < len(abbr):
             if abbr[abbr_pt].isdigit():
-                # leading zero
-                if abbr[abbr_pt] == '0': return False
-                shift = 0
+                steps = 0
+                # leading zero 
+                if abbr[abbr_pt] == '0':
+                    return False
                 while abbr_pt < len(abbr) and abbr[abbr_pt].isdigit():
-                    shift = (shift*10) + int(abbr[abbr_pt])
+                    steps = steps * 10 + int(abbr[abbr_pt])
                     abbr_pt += 1
-                str_pt += shift
+                word_pt += steps
             else:
-                if word[str_pt] != abbr[abbr_pt]: return False
-                str_pt += 1
-                abbr_pt += 1
+                if abbr[abbr_pt] != word[word_pt]: 
+                    return False
 
-        return str_pt == len(word) and abbr_pt == len(abbr)
+                abbr_pt += 1
+                word_pt += 1
+
+        return abbr_pt == len(abbr) and word_pt == len(word)
+
+
+            
+
+
+
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+        # str_pt = 0
+        # abbr_pt = 0
+
+        # while str_pt < len(word) and abbr_pt < len(abbr):
+        #     if abbr[abbr_pt].isdigit():
+        #         # leading zero
+        #         if abbr[abbr_pt] == '0': return False
+        #         shift = 0
+        #         while abbr_pt < len(abbr) and abbr[abbr_pt].isdigit():
+        #             shift = (shift*10) + int(abbr[abbr_pt])
+        #             abbr_pt += 1
+        #         str_pt += shift
+        #     else:
+        #         if word[str_pt] != abbr[abbr_pt]: return False
+        #         str_pt += 1
+        #         abbr_pt += 1
+
+        # return str_pt == len(word) and abbr_pt == len(abbr)
 
 
 
