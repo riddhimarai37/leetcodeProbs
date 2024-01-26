4a803b8821fd6dd60ef1e0d3f7456930d9c003c8
riddhimarai37 <57564654+riddhimarai37@users.noreply.github.com>
2024-01-26 17:39:25 -0500

    Time: 111 ms (51.69%) | Memory: 17 MB (77.10%) - LeetSync

--- 680-valid-palindrome-ii/valid-palindrome-ii.py
+++ 680-valid-palindrome-ii/valid-palindrome-ii.py
@@ -1,70 +1,26 @@
 class Solution:
     def validPalindrome(self, s: str) -> bool:
-
-        def isPalindrome(new_str):
-            if len(new_str) < 1: return True
-            left = 0
-            right = len(new_str) - 1
-
-            while left < right:
-                if new_str[left] != new_str[right]:
+        
+        def isPalindrome(left, right):
+            l = left
+            r = right
+            while l < r:
+                if s[l] != s[r]:
                     return False
-                left += 1
-                right -= 1
+                l += 1
+                r -=1 
             return True
 
-        if isPalindrome(s):
-            return True
 
-        left = 0
+        left = 0 
         right = len(s) - 1
+
 
         while left < right:
             if s[left] != s[right]:
-                return isPalindrome(s[:left] + s[left+1:]) or isPalindrome(s[:right] + s[right+1:])
+                return isPalindrome(left + 1, right) or isPalindrome(left, right-1)
             left += 1
             right -= 1
-            
 
-        
-
-
-        
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        
-        # def isPalindrome(left, right):
-        #     l = left
-        #     r = right
-        #     while l < r:
-        #         if s[l] != s[r]:
-        #             return False
-        #         l += 1
-        #         r -=1 
-        #     return True
-
-
-        # left = 0 
-        # right = len(s) - 1
-
-
-        # while left < right:
-        #     if s[left] != s[right]:
-        #         return isPalindrome(left + 1, right) or isPalindrome(left, right-1)
-        #     left += 1
-        #     right -= 1
-
-        # return True
+        return True
         
