08c724f964afb2fd6cfac24589b68029bfb2a55c
riddhimarai37 <57564654+riddhimarai37@users.noreply.github.com>
2024-01-26 17:37:42 -0500

    Time: 150 ms (18.03%) | Memory: 17 MB (77.10%) - LeetSync

--- 680-valid-palindrome-ii/valid-palindrome-ii.py
+++ 680-valid-palindrome-ii/valid-palindrome-ii.py
@@ -11,61 +11,60 @@ class Solution:
                     return False
                 left += 1
                 right -= 1
-            
             return True
 
-            if self.isPalindrome(s):
-                return True
-
-            left = 0
-            right = len(s) - 1
-
-            while left < right:
-                if s[left] != s[right]:
-                    return isPalindrome(s[:left] + s[left+1:]) or isPalindrome(s[:right] + s[right+1:])
-                left += 1
-                right -= 1
-            
-
-        
-
-
-        
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        
-        def isPalindrome(left, right):
-            l = left
-            r = right
-            while l < r:
-                if s[l] != s[r]:
-                    return False
-                l += 1
-                r -=1 
+        if isPalindrome(s):
             return True
 
-
-        left = 0 
+        left = 0
         right = len(s) - 1
-
 
         while left < right:
             if s[left] != s[right]:
-                return isPalindrome(left + 1, right) or isPalindrome(left, right-1)
+                return isPalindrome(s[:left] + s[left+1:]) or isPalindrome(s[:right] + s[right+1:])
             left += 1
             right -= 1
+            
 
-        return True
+        
+
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+        # def isPalindrome(left, right):
+        #     l = left
+        #     r = right
+        #     while l < r:
+        #         if s[l] != s[r]:
+        #             return False
+        #         l += 1
+        #         r -=1 
+        #     return True
+
+
+        # left = 0 
+        # right = len(s) - 1
+
+
+        # while left < right:
+        #     if s[left] != s[right]:
+        #         return isPalindrome(left + 1, right) or isPalindrome(left, right-1)
+        #     left += 1
+        #     right -= 1
+
+        # return True
         
