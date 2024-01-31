ab158201b29331c428c9a5f15fcbc6f142c6129a
riddhimarai37 <57564654+riddhimarai37@users.noreply.github.com>
2024-01-31 13:12:29 -0500

    Time: 1136 ms (40.76%) | Memory: 16.6 MB (85.06%) - LeetSync

--- 301-remove-invalid-parentheses/remove-invalid-parentheses.py
+++ 301-remove-invalid-parentheses/remove-invalid-parentheses.py
@@ -1,27 +1,46 @@
 class Solution:
-    
-    def dfs(self, s, depth, l, r, l_rmv, r_rmv, new_s):
-        if l_rmv < 0 or r_rmv < 0 or r > l:
-            return
-        if depth == len(s):
-            if l == r and l_rmv == 0 and r_rmv == 0:
-                self.ans.add(new_s)
+    def removeInvalidParentheses(self, s: str) -> List[str]:
+        self.longest_string = -1
+        self.res = set()
+
+        self.dfs(s, 0, [], 0, 0)
+        return self.res
+
+    # dfs backtracking 
+
+    def dfs(self, string, cur_idx, cur_res, left_count, right_count):
+        if cur_idx >= len(string):
+            if left_count == right_count:
+                if len(cur_res) > self.longest_string:
+                    self.longest_string = len(cur_res)
+                    # reset 
+                    self.res = set()
+                    self.res.add("".join(cur_res))
+                elif len(cur_res) == self.longest_string:
+                    self.res.add("".join(cur_res))
         else:
-            is_open, is_close = s[depth] == '(', s[depth] == ')'
-            self.dfs(s, depth + 1, l + is_open, r + is_close, l_rmv, r_rmv, new_s + s[depth])
-            if is_open or is_close:
-                self.dfs(s, depth + 1, l, r, l_rmv - is_open, r_rmv - is_close, new_s)
-    
-    def removeInvalidParentheses(self, s: str) -> List[str]:        
-        left, right = 0, 0
-        for char in s:
-            if char == '(':
-                left += 1
-            elif char == ')':
-                if left == 0:
-                    right += 1
-                else:
-                    left -= 1
-        self.ans = set()
-        self.dfs(s, 0, 0, 0, left, right, "")
-        return list(self.ans)
+            cur_char = string[cur_idx]
+
+            if cur_char == "(":
+                    # don't know if ")" exists later so must include this char
+                cur_res.append(cur_char)
+                self.dfs(string, cur_idx + 1, cur_res, left_count + 1, right_count)
+                cur_res.pop() # remove this char from current string
+
+                    # testing string without this char
+                self.dfs(string, cur_idx + 1, cur_res, left_count, right_count)
+            elif cur_char == ")":
+                    # run dfs with including this char and skipping it to see what's valid
+                self.dfs(string, cur_idx + 1, cur_res, left_count, right_count)
+
+                    # now testing including this char
+                if left_count > right_count:
+                        # including if we need another right paren
+                    cur_res.append(cur_char)
+                    self.dfs(string, cur_idx + 1, cur_res, left_count, right_count + 1)
+                    cur_res.pop()
+            else:
+                    # any other character
+                cur_res.append(cur_char)
+                self.dfs(string, cur_idx + 1, cur_res, left_count, right_count)
+                cur_res.pop()
