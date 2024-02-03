85c587a81ddba3153c866066adf7498c6a110877
riddhimarai37 <57564654+riddhimarai37@users.noreply.github.com>
2024-02-03 15:40:05 -0500

    Time: 94 ms (42.03%) | Memory: 19.7 MB (12.81%) - LeetSync

--- 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.py
+++ 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.py
@@ -1,4 +1,53 @@
+# left and right variables 
+# res = ""
+# first pass of s
+# if curr c is: res += c
+# if "(" --> add to res; left++ 
+# if ")" --> left < right == 1: add to res; --> right += 1
+
+# second pass of res
+# if curr char ==> move forward
+# if ")" --> move forward; right++
+# if "(" --> right > left: add to res
+
 class Solution:
+    def minRemoveToMakeValid(self, s:str) -> str:
+        left = right = 0
+        string_builder = []
+
+        # first pass 
+        for c in s:
+            if c.isalpha():
+                string_builder.append(c)
+            elif c == "(":
+                string_builder.append(c)
+                left += 1
+            else:
+                if left > right:
+                    string_builder.append(c)
+                    right += 1
+
+        # second pass 
+        res = []
+        left = right = 0
+        for idx in range(len(string_builder) - 1, -1, -1):
+            cur_char = string_builder[idx]
+            if cur_char.isalpha():
+                res.append(cur_char)
+            elif cur_char == ")":
+                right += 1
+                res.append(cur_char)
+            else:
+                if right > left:
+                    res.append("(")
+                    left += 1
+                else:
+                    continue
+
+        return "".join(reversed(res))
+
+
+
     # def minRemoveToMakeValid(self, s: str) -> str:
     #     # stack approach
     #     if len(s) == 1 and (s[0] == "(" or s[0] == ")"):
@@ -27,37 +76,37 @@ class Solution:
 
     # two pass approach
 
-    def minRemoveToMakeValid(self, s:str) -> str:
-        # pass 1: remove all invalid ")"
-        string_builder =[]
-        balance = 0
+    # def minRemoveToMakeValid(self, s:str) -> str:
+    #     # pass 1: remove all invalid ")"
+    #     string_builder =[]
+    #     balance = 0
 
-        for char in s:
-            if char == "(":
-                balance += 1
-            if char == ")":
-                if balance == 0:
-                    continue
-                balance -= 1
-            string_builder.append(char)
+    #     for char in s:
+    #         if char == "(":
+    #             balance += 1
+    #         if char == ")":
+    #             if balance == 0:
+    #                 continue
+    #             balance -= 1
+    #         string_builder.append(char)
 
-        if balance == 0:
-            return "".join(string_builder)
+    #     if balance == 0:
+    #         return "".join(string_builder)
 
-        # pass 2: remove all invalid "("
-        res = []
-        balance = 0
+    #     # pass 2: remove all invalid "("
+    #     res = []
+    #     balance = 0
 
-        for idx in range(len(string_builder)-1, -1, -1):
-            if string_builder[idx] == "(":
-                if balance == 0:
-                    continue
-                balance -= 1 
-            elif string_builder[idx] == ")":
-                balance += 1
-            res.append(string_builder[idx])
+    #     for idx in range(len(string_builder)-1, -1, -1):
+    #         if string_builder[idx] == "(":
+    #             if balance == 0:
+    #                 continue
+    #             balance -= 1 
+    #         elif string_builder[idx] == ")":
+    #             balance += 1
+    #         res.append(string_builder[idx])
 
-        return "".join(reversed(res))                
+    #     return "".join(reversed(res))                
 
 
 
