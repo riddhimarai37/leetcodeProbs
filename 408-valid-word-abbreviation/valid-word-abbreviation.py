f1fde24f001eb8b06576e7672cf0cbb31b3d8da1
riddhimarai37 <57564654+riddhimarai37@users.noreply.github.com>
2024-02-03 12:45:59 -0500

    Time: 32 ms (84.36%) | Memory: 16.6 MB (66.93%) - LeetSync

--- 408-valid-word-abbreviation/valid-word-abbreviation.py
+++ 408-valid-word-abbreviation/valid-word-abbreviation.py
@@ -1,32 +1,34 @@
+# two pointers 
+# keep iterating through word and abbr until we reach a non alpha character
+# if starts with 0 --> return false
+# o/w --> continue through abbr until we reach a character thats not a digit
+# int(number_str) 
+# move pointer num spaces forward and if teh current chars != e/0 then return false
+
 class Solution:
     def validWordAbbreviation(self, word: str, abbr: str) -> bool:
-        abbr_pt = word_pt = 0
+        pt1 = pt2 = 0
 
-        while word_pt < len(word) and abbr_pt < len(abbr):
-            if abbr[abbr_pt].isdigit():
-                steps = 0
-                # leading zero 
-                if abbr[abbr_pt] == '0':
-                    return False
-                while abbr_pt < len(abbr) and abbr[abbr_pt].isdigit():
-                    steps = steps * 10 + int(abbr[abbr_pt])
-                    abbr_pt += 1
-                word_pt += steps
+        while pt1 < len(word) and pt2 < len(abbr):
+            if abbr[pt2].isdigit():
+                if abbr[pt2] == '0':
+                        return False
+                number_str = ""
+                while pt2 < len(abbr) and abbr[pt2].isdigit():
+                    number_str += abbr[pt2]
+                    pt2 += 1
+
+                num = int(number_str)
+
+                pt1 += num
             else:
-                if abbr[abbr_pt] != word[word_pt]: 
+                if abbr[pt2] != word[pt1]:
                     return False
+                pt1 += 1
+                pt2 += 1
 
-                abbr_pt += 1
-                word_pt += 1
-
-        return abbr_pt == len(abbr) and word_pt == len(word)
-
-
-            
-
-
-
-            
+        return pt1 == len(word) and pt2 == len(abbr)
+                    
 
 
 
@@ -42,25 +44,46 @@ class Solution:
 
 
 
-        
-        # str_pt = 0
-        # abbr_pt = 0
 
-        # while str_pt < len(word) and abbr_pt < len(abbr):
+
+
+
+
+
+
+        # abbr_pt = word_pt = 0
+
+        # while word_pt < len(word) and abbr_pt < len(abbr):
         #     if abbr[abbr_pt].isdigit():
-        #         # leading zero
-        #         if abbr[abbr_pt] == '0': return False
-        #         shift = 0
+        #         steps = 0
+        #         # leading zero 
+        #         if abbr[abbr_pt] == '0':
+        #             return False
         #         while abbr_pt < len(abbr) and abbr[abbr_pt].isdigit():
-        #             shift = (shift*10) + int(abbr[abbr_pt])
+        #             steps = steps * 10 + int(abbr[abbr_pt])
         #             abbr_pt += 1
-        #         str_pt += shift
+        #         word_pt += steps
         #     else:
-        #         if word[str_pt] != abbr[abbr_pt]: return False
-        #         str_pt += 1
+        #         if abbr[abbr_pt] != word[word_pt]: 
+        #             return False
+
         #         abbr_pt += 1
+        #         word_pt += 1
 
-        # return str_pt == len(word) and abbr_pt == len(abbr)
+        # return abbr_pt == len(abbr) and word_pt == len(word)
+
+        # # Time = O(N)
+        # # Space = O(1)
+
+
+
+
+
+            
+
+
+
+            
 
 
 
@@ -68,4 +91,7 @@ class Solution:
 
 
 
-        
+
+
+
+
