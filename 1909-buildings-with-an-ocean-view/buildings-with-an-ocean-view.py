19701e6cb407e4c9d499ab5753ec9dca7a614342
riddhimarai37 <57564654+riddhimarai37@users.noreply.github.com>
2024-02-04 12:01:04 -0500

    Time: 481 ms (95.00%) | Memory: 33.4 MB (97.19%) - LeetSync

--- 1909-buildings-with-an-ocean-view/buildings-with-an-ocean-view.py
+++ 1909-buildings-with-an-ocean-view/buildings-with-an-ocean-view.py
@@ -1,14 +1,54 @@
 class Solution:
+    # traverse the array from the right and keep track of the current max height 
     def findBuildings(self, heights: List[int]) -> List[int]:
         max_height = 0
-        ptr = len(heights) - 1
-        res = []
+        res = collections.deque([])
 
-        while ptr >= 0:
-            if heights[ptr] > max_height:
-                res.append(ptr)
-                max_height = heights[ptr]
-            ptr -= 1
-            
-        res.reverse()
-        return res
+        for i in range(len(heights) - 1, - 1, - 1):
+            cur_height = heights[i]
+
+            if cur_height > max_height:
+                res.appendleft(i)
+                max_height = cur_height
+
+        return res   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        # max_height = 0
+        # ptr = len(heights) - 1
+        # res = []
+
+        # while ptr >= 0:
+        #     if heights[ptr] > max_height:
+        #         res.append(ptr)
+        #         max_height = heights[ptr]
+        #     ptr -= 1
+
+        # res.reverse()
+        # return res
+
+
+        # follow-up: just got a phone interview from Facebook. They asked this question, first  
+        # part was to count how many buildings have an ocean view. The second part was iterate  #from the beginning and return the list of indexes. I used stack for the second part.
+
+        
