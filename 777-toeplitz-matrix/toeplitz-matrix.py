9b9136e210ee433403a51b4e1242cdcee172a950
riddhimarai37 <57564654+riddhimarai37@users.noreply.github.com>
2024-02-04 12:58:26 -0500

    Time: 77 ms (75.27%) | Memory: 16.6 MB (59.04%) - LeetSync

--- 777-toeplitz-matrix/toeplitz-matrix.py
+++ 777-toeplitz-matrix/toeplitz-matrix.py
@@ -1,36 +1,11 @@
 class Solution:
-    # have a visited list with visited indices
-    # iterate through the matrix and continue going down a diagonal until we either see a nonequal value or we're outof bounds 
+    # optimal solution: compare every value to it's top-left neighbor: if not equal return false
     def isToeplitzMatrix(self, matrix: List[List[int]]) -> bool:
-        if len(matrix) == 1 or len(matrix[0]) == 1:
-            return True
-
-        visited = []
         for i in range(len(matrix)):
             for j in range(len(matrix[i])):
-                if (i,j) in visited:
-                    continue
+                # check if current point is part of a diagonal
+                if i > 0 and j > 0 and matrix[i][j] != matrix[i-1][j-1]:
+                    return False
 
-                val = matrix[i][j]
-                visited.append((i,j))
-
-                # check diagonals
-                row = i+1
-                col = j+1
-                while row < len(matrix) and col < len(matrix[i]):
-                    if (row,col) not in visited:
-                        if matrix[row][col] != val:
-                            return False
-                        visited.append((row,col))
-                    row += 1
-                    col += 1
-
-        return True 
-
-
-                
-                        
-
-
-
-        
+        return True
+             
