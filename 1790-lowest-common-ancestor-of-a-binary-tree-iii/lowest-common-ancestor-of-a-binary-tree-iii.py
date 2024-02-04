a40b1eb06334b5c68dbe98d02bc810bee84b7d64
riddhimarai37 <57564654+riddhimarai37@users.noreply.github.com>
2024-02-03 20:18:38 -0500

    Time: 40 ms (97.60%) | Memory: 19.9 MB (88.00%) - LeetSync

--- 1790-lowest-common-ancestor-of-a-binary-tree-iii/lowest-common-ancestor-of-a-binary-tree-iii.py
+++ 1790-lowest-common-ancestor-of-a-binary-tree-iii/lowest-common-ancestor-of-a-binary-tree-iii.py
@@ -8,22 +8,69 @@ class Node:
         self.parent = None
 """
 
+
+
 class Solution:
     def lowestCommonAncestor(self, p: 'Node', q: 'Node') -> 'Node':
         p_ptr = p
         q_ptr = q
 
         while p_ptr != q_ptr:
-            if p_ptr:
+            if p_ptr.parent:
                 p_ptr = p_ptr.parent
-            else: 
+            else:
                 p_ptr = q
-            if q_ptr:
+
+            if q_ptr.parent:
                 q_ptr = q_ptr.parent
             else:
                 q_ptr = p
-        
+
         return p_ptr
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        # p_ptr = p
+        # q_ptr = q
+
+        # while p_ptr != q_ptr:
+        #     if p_ptr:
+        #         p_ptr = p_ptr.parent
+        #     else: 
+        #         p_ptr = q
+        #     if q_ptr:
+        #         q_ptr = q_ptr.parent
+        #     else:
+        #         q_ptr = p
+        
+        # return p_ptr
+
+        # O(N) Time; O(1) Space
+
+        # Explanation: we need to eventually reach the shared lowest common ancestor
+        # if q and p are separate distances away from the lca, if we continue going up the tree
+        # and swap pointers when reaching null; we eventually reach a point where the pointers 
+        # converge and the parents equal each other; giving us the lca
         
 
 
