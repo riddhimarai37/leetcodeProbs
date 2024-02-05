18421a3c60974928063683f0b52d58060b4c0f9c
riddhimarai37 <57564654+riddhimarai37@users.noreply.github.com>
2024-02-05 09:27:14 -0500

    Time: 33 ms (80.93%) | Memory: 16.8 MB (50.21%) - LeetSync

--- 408-valid-word-abbreviation/valid-word-abbreviation.py
+++ 408-valid-word-abbreviation/valid-word-abbreviation.py
@@ -7,49 +7,49 @@
 
 class Solution:
     def validWordAbbreviation(self, word: str, abbr: str) -> bool:
-        pt1 = pt2 = 0
+        # pt1 = pt2 = 0
 
-        while pt1 < len(word) and pt2 < len(abbr):
-            if abbr[pt2].isdigit():
-                if abbr[pt2] == '0':
-                        return False
-                number_str = ""
-                while pt2 < len(abbr) and abbr[pt2].isdigit():
-                    number_str += abbr[pt2]
-                    pt2 += 1
+        # while pt1 < len(word) and pt2 < len(abbr):
+        #     if abbr[pt2].isdigit():
+        #         if abbr[pt2] == '0':
+        #                 return False
+        #         number_str = ""
+        #         while pt2 < len(abbr) and abbr[pt2].isdigit():
+        #             number_str += abbr[pt2]
+        #             pt2 += 1
 
-                num = int(number_str)
+        #         num = int(number_str)
 
-                pt1 += num
-            else:
-                if abbr[pt2] != word[pt1]:
-                    return False
-                pt1 += 1
-                pt2 += 1
-
-        return pt1 == len(word) and pt2 == len(abbr)
-
-
-        # abbr_pt = word_pt = 0
-
-        # while word_pt < len(word) and abbr_pt < len(abbr):
-        #     if abbr[abbr_pt].isdigit():
-        #         steps = 0
-        #         # leading zero 
-        #         if abbr[abbr_pt] == '0':
-        #             return False
-        #         while abbr_pt < len(abbr) and abbr[abbr_pt].isdigit():
-        #             steps = steps * 10 + int(abbr[abbr_pt])
-        #             abbr_pt += 1
-        #         word_pt += steps
+        #         pt1 += num
         #     else:
-        #         if abbr[abbr_pt] != word[word_pt]: 
+        #         if abbr[pt2] != word[pt1]:
         #             return False
+        #         pt1 += 1
+        #         pt2 += 1
 
-        #         abbr_pt += 1
-        #         word_pt += 1
+        # return pt1 == len(word) and pt2 == len(abbr)
 
-        # return abbr_pt == len(abbr) and word_pt == len(word)
+
+        abbr_pt = word_pt = 0
+
+        while word_pt < len(word) and abbr_pt < len(abbr):
+            if abbr[abbr_pt].isdigit():
+                steps = 0
+                # leading zero 
+                if abbr[abbr_pt] == '0':
+                    return False
+                while abbr_pt < len(abbr) and abbr[abbr_pt].isdigit():
+                    steps = steps * 10 + int(abbr[abbr_pt])
+                    abbr_pt += 1
+                word_pt += steps
+            else:
+                if abbr[abbr_pt] != word[word_pt]: 
+                    return False
+
+                abbr_pt += 1
+                word_pt += 1
+
+        return abbr_pt == len(abbr) and word_pt == len(word)
 
         # # Time = O(N)
         # # Space = O(1)
