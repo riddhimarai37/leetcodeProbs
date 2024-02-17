43e2be00be652a4920acf92bee43620a2cd764aa
riddhimarai37 <57564654+riddhimarai37@users.noreply.github.com>
2024-02-17 11:54:05 -0500

    Time: 30 ms (94.13%) | Memory: 16.6 MB (52.24%) - LeetSync

--- 314-binary-tree-vertical-order-traversal/binary-tree-vertical-order-traversal.py
+++ 314-binary-tree-vertical-order-traversal/binary-tree-vertical-order-traversal.py
@@ -5,18 +5,6 @@
 #         self.left = left
 #         self.right = right
 
-# [3,9,20,null,null, 15, 7]
-# return [[9], [3,15] , [20] , [7]]
-
-# cols = {col_idx}
-# res  = []
-# dfs(node, curr_col, minCol, maxCol):
-# if not node: return 
-# cols[curr_col] = node.val
-# dfs (node.left, curr_col - 1, minCol, maxCol)
-# dfs (node.right, curr_col + 1, minCol, maxCol)
-# dfs(root, 0, float(-inf), float(inf))
-# go through the dictionary through the ranges of minCol and maxCol and append the values to results
 
 class Solution:
     def verticalOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
@@ -24,21 +12,50 @@ class Solution:
             return []
 
         cols = defaultdict(list)
-        queue = deque([(root, 0)])  # (node, column)
-
+        queue = deque([(root, 0)])
+        minCol = maxCol = 0
+        
         while queue:
             node, col = queue.popleft()
-
             cols[col].append(node.val)
+
+            minCol = min(col, minCol)
+            maxCol = max(col, maxCol)
 
             if node.left:
                 queue.append((node.left, col - 1))
             if node.right:
                 queue.append((node.right, col + 1))
 
-        result = [cols[key] for key in range(min(cols), max(cols)+1)]
-        return result
+
+        res = []
+
+        for c in range(minCol, maxCol + 1):
+            res.append(cols[c])
+
+        return res
+
+       
+       
+        # if not root:
+        #     return []
+
+        # cols = defaultdict(list)
+        # queue = deque([(root, 0)])  # (node, column)
+
+        # while queue:
+        #     node, col = queue.popleft()
+
+        #     cols[col].append(node.val)
+
+        #     if node.left:
+        #         queue.append((node.left, col - 1))
+        #     if node.right:
+        #         queue.append((node.right, col + 1))
+
+        # result = [cols[key] for key in range(min(cols), max(cols)+1)]
+        # return result
             
 
-        # time O(N) space O(N)
+        # # time O(N) space O(N)
             
