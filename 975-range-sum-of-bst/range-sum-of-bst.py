e6e6c043844374c412135c9b6619a440e47090cb
riddhimarai37 <57564654+riddhimarai37@users.noreply.github.com>
2024-02-18 18:20:04 -0500

    Time: 106 ms (90.86%) | Memory: 23.6 MB (94.70%) - LeetSync

--- 975-range-sum-of-bst/range-sum-of-bst.py
+++ 975-range-sum-of-bst/range-sum-of-bst.py
@@ -6,16 +6,52 @@
 #         self.right = right
 class Solution:
     def rangeSumBST(self, root: Optional[TreeNode], low: int, high: int) -> int:
-        if not root:
-            return 0
 
-        if root.val >= low and root.val <= high:
-            return root.val + self.rangeSumBST(root.left,low, high) + self.rangeSumBST(root.right,low,high)
-        elif root.val < low:
-            return self.rangeSumBST(root.right, low, high) 
-        else:
-            return self.rangeSumBST(root.left, low, high)
+        def traverse(node, low, high):
+            if not node:
+                return 0
+
+            if node.val >= low and node.val <= high:
+                return node.val + traverse(node.left, low, high) + traverse(node.right, low, high)
+            elif node.val < low:
+                return traverse(node.right, low, high)
+            else:
+                return traverse(node.left, low, high)
+
+        return traverse(root, low, high)
+
+
+
+
+
 
         
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        # if not root:
+        #     return 0
+
+        # if root.val >= low and root.val <= high:
+        #     return root.val + self.rangeSumBST(root.left,low, high) + self.rangeSumBST(root.right,low,high)
+        # elif root.val < low:
+        #     return self.rangeSumBST(root.right, low, high) 
+        # else:
+        #     return self.rangeSumBST(root.left, low, high)
+
+        
+    # TIME O(N) Space O(N) b/c of the call stack
         
