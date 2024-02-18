1078b71340a68fa2ba09d259f9220647c1b4bbb0
riddhimarai37 <57564654+riddhimarai37@users.noreply.github.com>
2024-02-18 18:37:56 -0500

    Time: 56 ms (34.59%) | Memory: 21.1 MB (70.99%) - LeetSync

--- 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.py
+++ 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.py
@@ -9,6 +9,7 @@ class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
         if not root:
             return None
+
         if root == p or root == q:
             return root
 
@@ -19,6 +20,38 @@ class Solution:
             return root
         else:
             return left or right
+
+                
+
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+    #     if not root:
+    #         return None
+    #     if root == p or root == q:
+    #         return root
+
+    #     left = self.lowestCommonAncestor(root.left, p, q)
+    #     right = self.lowestCommonAncestor(root.right, p, q)
+
+    #     if left and right:
+    #         return root
+    #     else:
+    #         return left or right
+
+    # # TIME O(N) SPACE O(N) b/c of recursive stack
 
             
             
