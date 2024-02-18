b5ffc2ec7231cc6c24cefc2d18452a015c7887a7
riddhimarai37 <57564654+riddhimarai37@users.noreply.github.com>
2024-02-18 02:00:58 -0500

    Time: 144 ms (83.91%) | Memory: 21.1 MB (87.80%) - LeetSync

--- 912-random-pick-with-weight/random-pick-with-weight.py
+++ 912-random-pick-with-weight/random-pick-with-weight.py
@@ -1,57 +1,77 @@
 class Solution:
-
     def __init__(self, w: List[int]):
-        self.sums = []
-        self.prefix_sum = 0
+        self.total_sum = 0
+        self.pref_sums = []
         for weight in w:
-            self.prefix_sum += weight
-            self.sums.append(self.prefix_sum)
+            self.total_sum += weight
+            self.pref_sums.append(self.total_sum)
 
     def pickIndex(self) -> int:
-        if len(self.sums) == 1:
-            return 0
+        rand_num = random.random() * self.total_sum
 
-        target = random.random() * self.prefix_sum
-        # perform binary search to find where target falls
-        low = 0
-        high = len(self.sums)
-        while low < high:
-            mid = low + (high-low) // 2
-            if target > self.sums[mid]:
-                low = mid + 1
+        left = 0
+        right = len(self.pref_sums) - 1
+
+        while left < right:
+            mid = left + (right-left)//2
+            if rand_num > self.pref_sums[mid]:
+                left = mid + 1
             else:
-                high = mid
-        return low
+                right = mid
+        
+        return left
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    # def __init__(self, w: List[int]):
+    #     self.sums = []
+    #     self.prefix_sum = 0
+    #     for weight in w:
+    #         self.prefix_sum += weight
+    #         self.sums.append(self.prefix_sum)
+
+    # def pickIndex(self) -> int:
+    #     if len(self.sums) == 1:
+    #         return 0
+
+    #     target = random.random() * self.prefix_sum
+    #     # perform binary search to find where target falls
+    #     low = 0
+    #     high = len(self.sums)
+    #     while low < high:
+    #         mid = low + (high-low) // 2
+    #         if target > self.sums[mid]:
+    #             low = mid + 1
+    #         else:
+    #             high = mid
+    #     return low
 
 
 
     # Constructor Time = O(N), PickIndex Time = O(logN)
     # Constructor Space = O(N), PickIndex Space = O(1)
 
-        # self.prefix_sums = []
-        # pref_sum = 0
- 
-        # for weight in w:
-        #     pref_sum += weight
-        #     self.prefix_sums.append(pref_sum)
-        # self.total_sum = pref_sum
-
-
+    
 
 # Your Solution object will be instantiated and called as such:
 # obj = Solution(w)
 # param_1 = obj.pickIndex()
 
-        # rand_val = self.total_sum * random.random()
-        # # run a binary search to find the target zone
-        # low = 0
-        # high = len(self.prefix_sums)
-
-        # while low < high:
-        #     mid = low + (high-low) // 2
-        #     if rand_val > self.prefix_sums[mid]:
-        #         low = mid + 1
-        #     else:
-        #         high = mid
-
-        # return low
+    
