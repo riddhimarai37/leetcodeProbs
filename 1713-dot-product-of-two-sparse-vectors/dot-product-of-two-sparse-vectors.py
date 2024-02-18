bfad09b02d926e1df3e256f7c7a553fc15a10d6e
riddhimarai37 <57564654+riddhimarai37@users.noreply.github.com>
2024-02-17 20:31:48 -0500

    Time: 1283 ms (80.91%) | Memory: 21.2 MB (50.52%) - LeetSync

--- 1713-dot-product-of-two-sparse-vectors/dot-product-of-two-sparse-vectors.py
+++ 1713-dot-product-of-two-sparse-vectors/dot-product-of-two-sparse-vectors.py
@@ -1,38 +1,33 @@
 class SparseVector:
     def __init__(self, nums: List[int]):
-        self.values = []
-        for i,n in enumerate(nums):
+        self.new_nums = []
+        for idx, n in enumerate(nums):
             if n != 0:
-                self.values.append((i,n))
-    
+                self.new_nums.append((n, idx))
+
 
     # Return the dotProduct of two sparse vectors
     def dotProduct(self, vec: 'SparseVector') -> int:
-        dot_prod = 0
         pt1 = pt2 = 0
+        dot_prod = 0
+        
+        while pt1 < len(self.new_nums) and pt2 < len(vec.new_nums):
+            n1, idx1 = self.new_nums[pt1]
+            n2, idx2 = vec.new_nums[pt2]
 
-        while pt1 < len(self.values) and pt2 < len(vec.values):
-            i, num1 = self.values[pt1]
-            j, num2 = vec.values[pt2]
-
-            if i == j:
-                dot_prod += num1 * num2 
+            if idx1 == idx2:
+                dot_prod += n1 * n2
                 pt1 += 1
                 pt2 += 1
-            elif i > j:
+            elif idx1 > idx2:
                 pt2 += 1
-            else:
+            elif idx2 > idx1:
                 pt1 += 1
 
         return dot_prod
             
 
 
-# nums1 [1,0,0,2,3] values = [0,1], [2,2], [3,3]
- # num2 = [0,3,0,4,0] 
-
-
-                
 
 
 
@@ -49,38 +44,41 @@ class SparseVector:
 
 
 
-        
+
+
+
+
+
+    # def __init__(self, nums: List[int]):
+        # self.values = []
+        # for i,n in enumerate(nums):
+        #     if n != 0:
+        #         self.values.append((i,n))
+    
+
+    # Return the dotProduct of two sparse vectors
+    # def dotProduct(self, vec: 'SparseVector') -> int:
+        # dot_prod = 0
+        # pt1 = pt2 = 0
+
+        # while pt1 < len(self.values) and pt2 < len(vec.values):
+        #     i, num1 = self.values[pt1]
+        #     j, num2 = vec.values[pt2]
+
+        #     if i == j:
+        #         dot_prod += num1 * num2 
+        #         pt1 += 1
+        #         pt2 += 1
+        #     elif i > j:
+        #         pt2 += 1
+        #     else:
+        #         pt1 += 1
+
+        # return dot_prod
+            
 
 # Your SparseVector object will be instantiated and called as such:
 # v1 = SparseVector(nums1)
 # v2 = SparseVector(nums2)
 # ans = v1.dotProduct(v2)
 
-
-
-        # self.mult_values = []
-        # for idx,n in enumerate(nums):
-        #     if n != 0:
-        #         self.mult_values.append((n, idx))
-
-
-
-
-
-                # pt1 = pt2 = 0
-        # res = 0
-
-        # while pt1 < len(self.mult_values) and pt2 < len(vec.mult_values):
-        #     # if indices are equal add the dot product to 0
-        #     idx1 = self.mult_values[pt1][1]
-        #     idx2 = vec.mult_values[pt2][1]
-        #     if idx1 == idx2:
-        #         res += self.mult_values[pt1][0] * vec.mult_values[pt2][0]
-        #         pt1 += 1
-        #         pt2 += 1
-        #     elif idx1 > idx2:
-        #         pt2 +=1
-        #     else:
-        #         pt1 += 1
-
-        # return res
