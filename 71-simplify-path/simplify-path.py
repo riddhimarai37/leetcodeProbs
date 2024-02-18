ff3c00f74968da085375c78c93f29bfd56a04e62
riddhimarai37 <57564654+riddhimarai37@users.noreply.github.com>
2024-02-18 11:21:02 -0500

    Time: 35 ms (76.71%) | Memory: 16.7 MB (66.53%) - LeetSync

--- 71-simplify-path/simplify-path.py
+++ 71-simplify-path/simplify-path.py
@@ -1,18 +1,40 @@
 class Solution:
     def simplifyPath(self, path: str) -> str:
-        stack  = []
-
-        for i in path.split('/'):
-            # if i == "/" or i=="//" we skip 
-            if i == "..":
-                if stack:
-                    stack.pop()
-            elif i == "." or i == "":
-                # skip . or an empty string
+        stack = []
+        
+        for i in path.split("/"):
+            if i == "/" or i == "//" or i == "." or i == "":
                 continue
+            if i == "..":
+                if stack: stack.pop()
             else:
                 stack.append(i)
 
-        return "/" + "/".join(stack)                
+        return "/" + "/".join(stack)
+
+
+
+
+
+
+
+
+
+
+        
+        # stack  = []
+
+        # for i in path.split('/'):
+        #     # if i == "/" or i=="//" we skip 
+        #     if i == "..":
+        #         if stack:
+        #             stack.pop()
+        #     elif i == "." or i == "":
+        #         # skip . or an empty string
+        #         continue
+        #     else:
+        #         stack.append(i)
+
+        # return "/" + "/".join(stack)                
             
         
