e437b48fda5bda34cf10be498597bc155685795a
riddhimarai37 <57564654+riddhimarai37@users.noreply.github.com>
2024-02-20 15:19:48 -0500

    Time: 59 ms (9.55%) | Memory: 16.8 MB (55.17%) - LeetSync

--- 249-group-shifted-strings/group-shifted-strings.py
+++ 249-group-shifted-strings/group-shifted-strings.py
@@ -1,27 +1,87 @@
-# for strings to be in the same seq: 1. same length 2. subtract the ord(c) from e/o, if theyre the same then part of the sequence
-# first group the strings by length
-# 
+
 
 class Solution:
     def groupStrings(self, strings: List[str]) -> List[List[str]]:
-        seq_map = collections.defaultdict(list)
-        
-        for string in strings:
-            # edge case 
-            if len(string) == 1:
-                seq_map[(-1,)].append(string)
-            else:
-                char_diffs = []
-                i = 1
-                
-                while i < len(string):
-                    char_diffs.append((ord(string[i]) - ord(string[i-1])) % 26)
-                    i += 1
-                    
-                # key --> list of words with that sequence
-                seq_map[tuple(char_diffs)].append(string)
+        dic = collections.defaultdict(list)
+
+        for idx, s in enumerate(strings):
+            if len(dic) == 0:
+                dic[s].append(s)
+                continue
             
-        return seq_map.values()
+            equal = True
+
+            for key in dic.keys():
+                print(key, s)
+                if len(key) == len(s):
+                    equal = True
+                    diff = (ord(key[0]) - ord(s[0])) % 26
+                    i = 1
+                    while i < len(s):
+                        if (ord(key[i]) - ord(s[i])) % 26 != diff:
+                            equal = False
+                            break
+                        i += 1
+                    if equal:
+                        dic[key].append(s)
+                        break
+                else:
+                    equal = False
+            
+            if not equal:
+                dic[s].append(s)
+
+        return list(dic.values())
+
+
+                    
+                    
+                        
+                    
+
+
+
+                        
+
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        # seq_map = collections.defaultdict(list)
+        
+        # for string in strings:
+        #     # edge case 
+        #     if len(string) == 1:
+        #         seq_map[(-1,)].append(string)
+        #     else:
+        #         char_diffs = []
+        #         i = 1
+                
+        #         while i < len(string):
+        #             char_diffs.append((ord(string[i]) - ord(string[i-1])) % 26)
+        #             i += 1
+                    
+        #         # key --> list of words with that sequence
+        #         seq_map[tuple(char_diffs)].append(string)
+            
+        # return seq_map.values()
 
         # Time O(N * k); n= number of strings given, k is the length of each string
         # Space: O(N * K)
