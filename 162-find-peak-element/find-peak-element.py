a488083dd2e77bd9c080ccd6944edaab5e5ec9ec
riddhimarai37 <57564654+riddhimarai37@users.noreply.github.com>
2024-02-20 00:18:13 -0500

    Time: 54 ms (18.69%) | Memory: 16.7 MB (65.95%) - LeetSync

--- 162-find-peak-element/find-peak-element.py
+++ 162-find-peak-element/find-peak-element.py
@@ -1,25 +1,60 @@
 class Solution:
     def findPeakElement(self, nums: List[int]) -> int:
         if len(nums) == 1:
-            return 0 
-        left = 0 
+            return 0
+
+        left = 0
         right = len(nums) - 1
 
         while left < right:
             if left == 0 and nums[left] > nums[left+1]:
                 return left
             if right == len(nums) - 1 and nums[right] > nums[right-1]:
-                return right
+                return right 
 
-            mid = left + (right-left) // 2
+            mid = left + (right-left)//2
 
-            if nums[mid] > nums[mid-1] and nums[mid] > nums[mid+1]:
+            if nums[mid] > nums[mid+1] and nums[mid] > nums[mid-1]:
                 return mid
-            
-            if nums[mid+1] > nums[mid]:
+
+            if nums[mid] < nums[mid+1]:
                 left = mid
             else:
                 right = mid
+          
+
+
+
+
+
+
+
+
+
+
+
+        # if len(nums) == 1:
+        #     return 0 
+        # left = 0 
+        # right = len(nums) - 1
+
+        # while left < right:
+        #     if left == 0 and nums[left] > nums[left+1]:
+        #         return left
+        #     if right == len(nums) - 1 and nums[right] > nums[right-1]:
+        #         return right
+
+        #     mid = left + (right-left) // 2
+
+        #     if nums[mid] > nums[mid-1] and nums[mid] > nums[mid+1]:
+        #         return mid
+            
+        #     if nums[mid+1] > nums[mid]:
+        #         left = mid
+        #     else:
+        #         right = mid
+
+        # # TIME O(logn) SPACE O(1)
 
             
 
