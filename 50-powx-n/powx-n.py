c4a6c334d600cba9828e58a0de64cc16621bbec0
riddhimarai37 <57564654+riddhimarai37@users.noreply.github.com>
2024-02-20 00:40:07 -0500

    Time: 22 ms (99.48%) | Memory: 16.6 MB (84.89%) - LeetSync

--- 50-powx-n/powx-n.py
+++ 50-powx-n/powx-n.py
@@ -1,5 +1,60 @@
 class Solution:
     def myPow(self, x: float, n: int) -> float:
+
+        def helper(num, power):
+            if power == 0:
+                return 1
+            if power < 0:
+                power = -power
+                num = 1.0/num
+
+            res = 1
+
+            while power != 0:
+                if power % 2 == 1:
+                    res *= num 
+                    power -= 1
+                num *= num
+                power //= 2
+            
+            return res
+
+        return helper(x, float(n))
+                
+
+                
+
+                
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         # if n == 0:
         #     return 1
         # elif n < 0:
@@ -14,25 +69,27 @@ class Solution:
 
 
         # OPT APPROACH (binary exponentiation)
-        def binaryExp(x,n):
-            if n == 0:
-                return 1
-            if n < 0:
-                n = -n
-                x = 1.0/x
+        # def binaryExp(x,n):
+        #     if n == 0:
+        #         return 1
+        #     if n < 0:
+        #         n = -n
+        #         x = 1.0/x
 
-            res = 1
+        #     res = 1
 
-            while n != 0:
-                if n%2 == 1:
-                    res *= x 
-                    n -= 1
-                x *= x
-                n = n//2
+        #     while n != 0:
+        #         if n%2 == 1:
+        #             res *= x 
+        #             n -= 1
+        #         x *= x
+        #         n = n//2
             
-            return res
+        #     return res
 
-        return binaryExp(x, float(n))
+        # return binaryExp(x, float(n))
+
+        # # TIME O(logN) SPACE O(1)
         
 
 
