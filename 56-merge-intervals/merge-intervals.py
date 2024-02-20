3636cbde1368a020b106e570163dccf96266dd22
riddhimarai37 <57564654+riddhimarai37@users.noreply.github.com>
2024-02-20 18:41:42 -0500

    Time: 123 ms (65.60%) | Memory: 21 MB (62.53%) - LeetSync

--- 56-merge-intervals/merge-intervals.py
+++ 56-merge-intervals/merge-intervals.py
@@ -1,30 +1,26 @@
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        # sort the intervals by starting value
         if len(intervals) == 1:
             return intervals
 
-        # sort intervals by start values 
-        intervals = sorted(intervals, key=lambda x: x[0])
-        res = []
-        left = 0
-        right = 1
+        intervals = sorted(intervals, key= lambda x: x[0])
+        res = [intervals[0]]
+        i = 1
 
-        while right <= len(intervals):
-            # merge the intervals
-            while right < len(intervals) and intervals[left][1] >= intervals[right][0]:
-                start1 = intervals[left][0]
-                start2 = intervals[right][0]
-                end1 = intervals[left][1]
-                end2 = intervals[right][1]
+        while i < len(intervals):
+            curr_start, curr_end = res[-1]
 
-                intervals[left] = [min(start1, start2), max(end1, end2)]
-                right += 1
-            # update with merged interval
-            res.append(intervals[left])
-            left = right
-            right += 1
+            if curr_end >= intervals[i][0]:
+                res[-1][0] = min(curr_start, intervals[i][0])
+                res[-1][1] = max(curr_end, intervals[i][1])
+            else:
+                res.append([intervals[i][0], intervals[i][1]])
+
+            i += 1
 
         return res
+        
 
 
 
@@ -44,6 +40,43 @@ class Solution:
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+        # if len(intervals) == 1:
+        #     return intervals
+
+        # # sort intervals by start values 
+        # intervals = sorted(intervals, key=lambda x: x[0])
+        # res = []
+        # left = 0
+        # right = 1
+
+        # while right <= len(intervals):
+        #     # merge the intervals
+        #     while right < len(intervals) and intervals[left][1] >= intervals[right][0]:
+        #         start1 = intervals[left][0]
+        #         start2 = intervals[right][0]
+        #         end1 = intervals[left][1]
+        #         end2 = intervals[right][1]
+
+        #         intervals[left] = [min(start1, start2), max(end1, end2)]
+        #         right += 1
+        #     # update with merged interval
+        #     res.append(intervals[left])
+        #     left = right
+        #     right += 1
+
+        # return res
 
 
 
