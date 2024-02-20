d9cdedd027eba83f39a09d21cb468ce50c212413
riddhimarai37 <57564654+riddhimarai37@users.noreply.github.com>
2024-02-20 18:26:38 -0500

    Time: 43 ms (22.07%) | Memory: 16.5 MB (95.72%) - LeetSync

--- 199-binary-tree-right-side-view/binary-tree-right-side-view.py
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.py
@@ -4,55 +4,100 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-# class Solution:
-#     def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
-#         from collections import deque
+class Solution:
+    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+        if not root:
+            return []
+
+        res = []
+
+        def dfs(node, row):
+            if not node:
+                return 
+
+            if len(res) - 1 < row:
+                res.append(node.val)
+
+            if node.right:
+                dfs(node.right, row + 1)
+            if node.left:
+                dfs(node.left, row + 1)
+
+            return res
+
+        return dfs(root, 0)
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        # from collections import deque
         
-#         if not root:
-#             return []
+        # if not root:
+        #     return []
 
-#         rows = []
-#         queue = deque([(0,root)])
+        # rows = []
+        # queue = deque([(0,root)])
 
-#         while queue:
-#             row, node = queue.popleft()
-#             if len(rows) <= row:
-#                 rows.append([node.val])
-#             else:
-#                 rows[row].append(node.val)
+        # while queue:
+        #     row, node = queue.popleft()
+        #     if len(rows) <= row:
+        #         rows.append([node.val])
+        #     else:
+        #         rows[row].append(node.val)
 
-#             if node.left: 
-#                 queue.append((row + 1, node.left))
-#             if node.right:
-#                 queue.append((row+1, node.right))
+        #     if node.left: 
+        #         queue.append((row + 1, node.left))
+        #     if node.right:
+        #         queue.append((row+1, node.right))
 
-#         res = []
-#         for curr in rows:
-#             # add right most value to res
-#             res.append(curr[-1])
+        # res = []
+        # for curr in rows:
+        #     # add right most value to res
+        #     res.append(curr[-1])
 
-#         return res
+        # return res
 
 
 # neetcode solution 
-class Solution:
-    def rightSideView(self, root: TreeNode) -> List[int]:
-        res = []
-        q = collections.deque([root])
+# class Solution:
+#     def rightSideView(self, root: TreeNode) -> List[int]:
+#         res = []
+#         q = collections.deque([root])
 
-        while q:
-            rightSide = None
-            qLen = len(q)
+#         while q:
+#             rightSide = None
+#             qLen = len(q)
 
-            for i in range(qLen):
-                node = q.popleft()
-                if node:
-                    rightSide = node
-                    q.append(node.left)
-                    q.append(node.right)
-            if rightSide:
-                res.append(rightSide.val)
-        return res
+#             for i in range(qLen):
+#                 node = q.popleft()
+#                 if node:
+#                     rightSide = node
+#                     q.append(node.left)
+#                     q.append(node.right)
+#             if rightSide:
+#                 res.append(rightSide.val)
+#         return res
 
 
 
