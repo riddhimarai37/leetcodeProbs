8c0eb676bb771fcb2d60ee538a05ccae5eb9a700
riddhimarai37 <57564654+riddhimarai37@users.noreply.github.com>
2024-02-20 23:46:21 -0500

    Time: 48 ms (17.57%) | Memory: 18.1 MB (59.13%) - LeetSync

--- 270-closest-binary-search-tree-value/closest-binary-search-tree-value.py
+++ 270-closest-binary-search-tree-value/closest-binary-search-tree-value.py
@@ -6,25 +6,61 @@
 #         self.right = right
 class Solution:
     def closestValue(self, root: Optional[TreeNode], target: float) -> int:
-        self.min_diff = float('inf')
-        self.min_diff_value = -1
+        ans = root.val
+        while(root):
+            if abs(target - root.val) < abs(target - ans):
+                ans = root.val
+            elif abs(target - root.val) == abs(target - ans):
+                ans = min(root.val, ans)
+
+            if target > root.val:
+                root = root.right
+            else:
+                root = root.left
+        return ans
+            
 
 
-        def traverse(node):
-            if not node:
-                return 0
 
-            curr_diff = abs(target - node.val)
 
-            if curr_diff < self.min_diff:
-                self.min_diff = curr_diff
-                self.min_diff_value = node.val
-            elif curr_diff == self.min_diff:
-                self.min_diff_value = min(node.val, self.min_diff_value)
-                
-            traverse(node.left)
-            traverse(node.right)
 
-        traverse(root)
-        return self.min_diff_value
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        # self.min_diff = float('inf')
+        # self.min_diff_value = -1
+
+        # def traverse(node):
+        #     if not node:
+        #         return 0
+
+        #     curr_diff = abs(target - node.val)
+
+        #     if curr_diff < self.min_diff:
+        #         self.min_diff = curr_diff
+        #         self.min_diff_value = node.val
+        #     elif curr_diff == self.min_diff:
+        #         self.min_diff_value = min(node.val, self.min_diff_value)
+
+        #     traverse(node.left)
+        #     traverse(node.right)
+
+        # traverse(root)
+        # return self.min_diff_value
         
+        # o(n) time and space solutions 
+
+
