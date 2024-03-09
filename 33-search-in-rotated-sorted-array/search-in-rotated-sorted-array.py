83e91f5fe1bde75c8de112c91a3b54419034217e
riddhimarai37 <57564654+riddhimarai37@users.noreply.github.com>
2024-03-09 17:13:47 -0500

    Time: 50 ms (14.92%) | Memory: 16.9 MB (63.04%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
@@ -1,66 +1,29 @@
-# class Solution:
-#     def search(self, nums: List[int], target: int) -> int:
-#         if len(nums) == 1 and nums[0] == target: 
-#             return 0
-#         left = 0
-#         right = len(nums) - 1
-
-#         while left < right:
-#             mid = left + (right-left) // 2
-
-#             if nums[mid] == target:
-#                 return mid
-#             elif nums[left] == target:
-#                 return left
-#             elif nums[right] == target:
-#                 return right
-
-#             if nums[left] < nums[mid]:
-#                 if nums[left] < target < nums[mid]:
-#                     right = mid - 1
-#                     continue
-#                 else:
-#                     left = mid + 1
-#                     continue
-#             else:
-#                 if nums[mid] < target < nums[right]:
-#                     left = mid + 1
-#                     continue
-#                 else:
-#                     right = mid - 1
-#                     continue
-
-#         return -1
-        
-
-
-        # neetcode (cleaner solution)
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        left = 0
-        right = len(nums) - 1
+        idx = -1
+        low = 0 
+        high = len(nums) - 1
 
-        while left <= right:
-            mid = left + (right - left) // 2
-
+        while low <= high:
+            mid = low + (high-low) // 2
             if nums[mid] == target:
                 return mid
 
             # left sorted portion
-            if nums[left] <= nums[mid]:
-                if target > nums[mid] or target < nums[left]:
-                    left = mid + 1
+            if nums[low] <= nums[mid]:
+                if target >= nums[low] and target <= nums[mid]:
+                    high = mid -1
                 else:
-                    right = mid - 1
-
+                    low = mid + 1
             # right sorted portion
             else:
-                if target < nums[mid] or target > nums[right]:
-                    right = mid - 1
+                if target >= nums[mid] and target <= nums[high]:
+                    low = mid + 1
                 else:
-                    left = mid + 1
+                    high = mid -1
 
-        return -1
+        return idx
 
+            
 
-
+            
