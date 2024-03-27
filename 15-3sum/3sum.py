127963b8cc453f40e5eea49bb071a1cef9849aad
riddhimarai37 <57564654+riddhimarai37@users.noreply.github.com>
2024-03-27 12:35:28 -0400

    Time: 524 ms (94.58%) | Memory: 20.8 MB (50.15%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -1,39 +1,67 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
+        ans = []
         nums.sort()
-        res = []
 
-        for i,num in enumerate(nums):
+        for idx, num in enumerate(nums):
             if num > 0:
                 break
 
-            # if you come across a duplicate
-            if i > 0 and num == nums[i-1]:
+            if idx > 0 and num == nums[idx-1]:
                 continue
 
-            # find two nums that add up to 0 with num
-            l = i + 1
+            l = idx + 1
             r = len(nums) - 1
-            while l < r:
-                threeSum = num + nums[l] + nums[r]
 
-                if threeSum > 0:
+            while l < r:
+                three_sum = num + nums[l] + nums[r]
+
+                # if sum is positive
+                if three_sum > 0:
                     r -= 1
-                elif threeSum < 0:
+                # if sum is negative
+                elif three_sum < 0:
                     l += 1
+                # if sum == 0
                 else:
-                    res.append([num, nums[l], nums[r]])
+                    ans.append([num, nums[l], nums[r]])
                     l += 1
-                    r-=1 
-                    # skip over duplicates
+                    r -= 1
                     while nums[l] == nums[l-1] and l < r:
                         l += 1
 
-        return res
-            
+        return ans
 
+                
 
 
         
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
