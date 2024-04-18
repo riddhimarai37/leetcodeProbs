6ca936c57a82f9d7af12c079a45c9dee7232c56a
riddhimarai37 <57564654+riddhimarai37@users.noreply.github.com>
2024-04-18 14:59:47 -0400

    Time: 37 ms (46.77%) | Memory: 16.6 MB (74.97%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.py
+++ 20-valid-parentheses/valid-parentheses.py
@@ -1,5 +1,46 @@
-# class Solution:
-#     def isValid(self, s: str) -> bool:
+class Solution:
+    def isValid(self, s: str) -> bool:
+        if len(s) == "":
+            return True
+
+        stack = []
+
+        for c in s:
+            if c in ["(", "{", "["]:
+                stack.append(c)
+            else:
+                if not stack:
+                    return False
+                popped_brack = stack.pop()
+                if c == ")" and popped_brack != "(":
+                    return False
+                elif c == "}" and popped_brack != "{":
+                    return False
+                elif c == "]" and popped_brack != "[":
+                    return False
+
+        return not stack
+
+                
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
 #         if len(s) == 1: return False
 
 #         stack = []
@@ -20,22 +61,22 @@
 #         return True if len(stack) == 0 else False
 
 
-# neetcode soln 
+# # neetcode soln 
 
-class Solution:
-    def isValid(self, s: str) -> bool:
-        Map = {")": "(", "]": "[", "}": "{"}
-        stack = []
+# class Solution:
+#     def isValid(self, s: str) -> bool:
+#         Map = {")": "(", "]": "[", "}": "{"}
+#         stack = []
 
-        for c in s:
-            if c not in Map: # c is an open parentheses 
-                stack.append(c)
-                continue
-            if not stack or stack[-1] != Map[c]: # not an open parantheses and nothing to pop from stack
-                return False
-            stack.pop()
+#         for c in s:
+#             if c not in Map: # c is an open parentheses 
+#                 stack.append(c)
+#                 continue
+#             if not stack or stack[-1] != Map[c]: # not an open parantheses and nothing to pop from stack
+#                 return False
+#             stack.pop()
 
-        return not stack
+#         return not stack
 
 
 
