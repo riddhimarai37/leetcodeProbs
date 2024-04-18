12649f4532d290b52e23affd66b47f1491f15aa2
riddhimarai37 <57564654+riddhimarai37@users.noreply.github.com>
2024-04-18 00:24:35 -0400

    Time: 118 ms (33.14%) | Memory: 16.6 MB (49.32%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
@@ -1,19 +1,53 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
-        char_map = {}
+        count = {}
+        left = 0
         res = 0
 
-        l = 0
-        for r in range(len(s)):
-            char_map[s[r]] = 1 + char_map.get(s[r], 0)
+        for right in range(len(s)):
+            # increment right count
+            count[s[right]] = 1 + count.get(s[right], 0)
 
-            # checking if current window is valid; if not shift
-            # r-l+1 - max(char_map.values()) == number of replacement
-            if (r-l+1) - max(char_map.values()) > k :
-                char_map[s[l]] -= 1
-                l += 1
+            # checking if current window is valid
+            while right-left+1 - max(count.values()) > k:
+                count[s[left]] -= 1
+                left += 1
 
-            # max is size of the windwo or previous max
-            res = max(res,r-l+1)
+            res = max(res, right - left + 1)
 
         return res
+
+
+
+        # O(n) or O(26 * n) time and O(1) space bc of char map
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+        # char_map = {}
+        # res = 0
+
+        # l = 0
+        # for r in range(len(s)):
+        #     char_map[s[r]] = 1 + char_map.get(s[r], 0)
+
+        #     # checking if current window is valid; if not shift
+        #     # r-l+1 - max(char_map.values()) == number of replacement
+        #     if (r-l+1) - max(char_map.values()) > k :
+        #         char_map[s[l]] -= 1
+        #         l += 1
+
+        #     # max is size of the windwo or previous max
+        #     res = max(res,r-l+1)
+
+        # return res
