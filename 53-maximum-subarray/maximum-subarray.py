b57c48ba65bf2e2ce559e23d668e5cf1aea480fd
riddhimarai37 <57564654+riddhimarai37@users.noreply.github.com>
2024-04-19 08:57:27 -0400

    Time: 519 ms (71.22%) | Memory: 30.5 MB (98.14%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.py
+++ 53-maximum-subarray/maximum-subarray.py
@@ -3,13 +3,40 @@ class Solution:
         max_sum = nums[0]
         curr_sum = 0
 
-        for num in nums:
+        for n in nums:
             if curr_sum < 0:
                 curr_sum = 0
-            curr_sum += num
-            max_sum = max(max_sum, curr_sum)
+            curr_sum += n
+            max_sum = max(curr_sum, max_sum)
 
-        return max_sum 
+        return max_sum
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+        # max_sum = nums[0]
+        # curr_sum = 0
+
+        # for num in nums:
+        #     if curr_sum < 0:
+        #         curr_sum = 0
+        #     curr_sum += num
+        #     max_sum = max(max_sum, curr_sum)
+
+        # return max_sum 
 
         #Time: O(n) Space: O(1)
 
