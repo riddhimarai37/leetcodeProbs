6094333a62ce0382314313e35a01df749f2a4533
riddhimarai37 <57564654+riddhimarai37@users.noreply.github.com>
2024-04-19 08:33:18 -0400

    Time: 28 ms (90.99%) | Memory: 16.5 MB (30.80%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.py
+++ 70-climbing-stairs/climbing-stairs.py
@@ -1,16 +1,16 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
         # edge case
-        if n <= 3:
-            return n
-        n1, n2 = 2,3
+        # if n <= 3:
+        #     return n
+        # n1, n2 = 2,3
         one,two = 1,1
 
-        for i in range(4, n+1):
-            temp = n1 + n2
-            n1 = n2
-            n2 = temp
+        for i in range(n-1):
+            temp = one 
+            one = one + two
+            two = temp
 
-        return n2
+        return one
         
         
