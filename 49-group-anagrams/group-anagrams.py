e6552a139e80c93e18762de74a397fc5f8e57966
riddhimarai37 <57564654+riddhimarai37@users.noreply.github.com>
2024-04-19 01:26:42 -0400

    Time: 104 ms (15.50%) | Memory: 21.2 MB (31.27%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -1,9 +1,61 @@
 class Solution:
     def groupAnagrams(self, strs):
-        anagram_map = defaultdict(list)
+        count_dicts = {}
+        res = []
+
+        for s in strs:
+            count = [0] * 26 
+            # populate the count dict
+            for c in s:
+                count[ord(c) - ord('a')] += 1
+
+            if tuple(count) in count_dicts:
+                count_dicts[tuple(count)].append(s)
+            else:
+                count_dicts[tuple(count)] = [s]
+
+        for count in count_dicts:
+            res.append(count_dicts[tuple(count)])
+
+        return res
         
-        for word in strs:
-            sorted_word = ''.join(sorted(word))
-            anagram_map[sorted_word].append(word)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         
-        return list(anagram_map.values())
+        # anagram_map = defaultdict(list)
+        
+        # for word in strs:
+        #     sorted_word = ''.join(sorted(word))
+        #     anagram_map[sorted_word].append(word)
+        
+        # return anagram_map.values()
+        # # O(m*nlogn)
+
+
+# neetcode solution
+# class Solution:
+#     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+#         ans = collections.defaultdict(list)
+
+#         for s in strs:
+#             count = [0] * 26
+#             for c in s:
+#                 count[ord(c) - ord("a")] += 1
+#             ans[tuple(count)].append(s)
+#         return ans.values()
+
+    # 0(mn) time complexity
