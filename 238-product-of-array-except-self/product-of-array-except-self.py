4a6eb96153ddd77fbf004201a1bf48f695f9ca7d
riddhimarai37 <57564654+riddhimarai37@users.noreply.github.com>
2024-04-19 08:49:03 -0400

    Time: 290 ms (5.54%) | Memory: 25.7 MB (20.34%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.py
+++ 238-product-of-array-except-self/product-of-array-except-self.py
@@ -1,25 +1,29 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        ans = []
-        prefix_prod = 1
-        i = 0
-        j = len(nums) - 1
+        ans = [1] * len(nums)
 
-        while i < len(nums):
-            ans.append(prefix_prod)
-            prefix_prod *= nums[i]
-            i += 1
+        prefix = 1
+        for idx in range(len(nums)):
+            ans[idx] = prefix
+            prefix *= nums[idx]
 
-
-        postfix_prod = 1
-
-        while j >= 0:
-            ans[j] *= postfix_prod
-            postfix_prod *= nums[j]
-            j -= 1
-
+        postfix = 1
+        for idx in range(len(nums)-1, -1, -1):
+            ans[idx] *= postfix
+            postfix *= nums[idx]
 
         return ans
+
+
+
+
+        
+
+
+
+
+
+
 
 
 
@@ -36,22 +40,23 @@ class Solution:
 
         
         # ans = []
-        # prefix = nums[0]
-        # idx = 1
-        # ans.append(1)
+        # prefix_prod = 1
+        # i = 0
+        # j = len(nums) - 1
 
-        # while idx < len(nums):
-        #     ans.append(prefix)
-        #     prefix *= nums[idx]
-        #     idx += 1
+        # while i < len(nums):
+        #     ans.append(prefix_prod)
+        #     prefix_prod *= nums[i]
+        #     i += 1
 
-        # postfix = nums[-1]
-        # idx = len(nums) - 2
 
-        # while idx >= 0:
-        #     ans[idx] *= postfix
-        #     postfix *= nums[idx]
-        #     idx -= 1
+        # postfix_prod = 1
+
+        # while j >= 0:
+        #     ans[j] *= postfix_prod
+        #     postfix_prod *= nums[j]
+        #     j -= 1
+
 
         # return ans
 
