f6ccdec0f1b8a85921c1d2a1451c25fa93248450
riddhimarai37 <57564654+riddhimarai37@users.noreply.github.com>
2024-06-05 14:01:55 -0400

    Time: 41 ms (36.72%) | Memory: 16.5 MB (51.13%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.py
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.py
@@ -3,30 +3,67 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+
+# Iterative
 class Solution:
-    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+    def mergeTwoLists(self, list1: ListNode, list2: ListNode) -> ListNode:
+        temp = res = ListNode()
 
-        if not list1 and not list2:
-            return list1
-        if not list1 and list2:
-            return list2
-        if list1 and not list2:
-            list1
-
-        temp = result = ListNode()
-        while list1 != None and list2 != None:
-            if list1.val <= list2.val:
-                result.next = list1 
+        while list1 and list2:
+            if list1.val < list2.val:
+                res.next = list1
+                res = res.next
                 list1 = list1.next
             else:
-                result.next = list2
+                res.next = list2
+                res = res.next
                 list2 = list2.next
-    
-            result = result.next
 
         if list1:
-            result.next = list1
-        if list2:
-            result.next = list2
+            res.next = list1
+        elif list2:
+            res.next = list2
 
         return temp.next
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+#         dummy = node = ListNode()
+
+#         while list1 and list2:
+#             if list1.val < list2.val:
+#                 node.next = list1
+#                 list1 = list1.next
+#             else:
+#                 node.next = list2
+#                 list2 = list2.next
+#             node = node.next
+
+#         node.next = list1 or list2
+
+#         return dummy.next
+    
+# # Recursive
+# class Solution:
+#     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+#         if not list1:
+#             return list2
+#         if not list2:
+#             return list1
+#         lil, big = (list1, list2) if list1.val < list2.val else (list2, list1)
+#         lil.next = self.mergeTwoLists(lil.next, big)
+#         return lil
