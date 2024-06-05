e6e121c141b2b33a44324b18f16db38b6aa423b3
riddhimarai37 <57564654+riddhimarai37@users.noreply.github.com>
2024-06-05 17:34:26 -0400

    Time: 34 ms (96.53%) | Memory: 17 MB (87.49%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -1,75 +1,73 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        s = s.lower()
         left = 0 
         right = len(s) - 1
+        s = s.lower()
 
         while left < right:
-            if s[left].isalnum() and s[right].isalnum() and s[left] != s[right]:
-                return False
-            elif not s[left].isalnum():
+            if not s[left].isalnum():
                 left += 1
             elif not s[right].isalnum():
                 right -= 1
             else:
+                if s[left] != s[right]:
+                    return False
+            
                 left += 1
                 right -= 1
 
         return True
-            
-
-            
-
-            
 
 
 
 
 
-        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        # s = s.lower()
         # left = 0 
         # right = len(s) - 1
 
         # while left < right:
-        #     if s[left].isalnum() and s[right].isalnum() and s[left].lower() != s[right].lower():
+        #     if s[left].isalnum() and s[right].isalnum() and s[left] != s[right]:
         #         return False
-        #     elif not s[left].isalnum(): 
+        #     elif not s[left].isalnum():
         #         left += 1
-        #     elif not s[right].isalnum(): 
-        #         right -=1 
+        #     elif not s[right].isalnum():
+        #         right -= 1
         #     else:
         #         left += 1
-        #         right -=1
+        #         right -= 1
 
         # return True
 
 
-
-
-
-
-
-#         if len(s) < 2: return True
-
-#         s = s.lower()
-#         left = 0
-#         right = len(s) - 1
-
-#         while left < right:
-#             if not s[left].isalnum():
-#                 left += 1
-#                 continue
-#             if not s[right].isalnum():
-#                 right -= 1    
-#                 continue 
-
-#             if s[left] != s[right]:
-#                 return False
+        # Time O(n) Space: O(1)
             
-#             left += 1
-#             right -=1
 
-#         return True
+            
+
+            
+
+
+
+
+
+
+
+
 
 # # neetcode solution # 1 -- uses extra space
 
