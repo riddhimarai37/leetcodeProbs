631525ba11835bc924b69a464fa3508f363497c4
riddhimarai37 <57564654+riddhimarai37@users.noreply.github.com>
2024-06-19 11:53:13 -0400

    Time: 48 ms (5.03%) | Memory: 16.5 MB (89.92%) - LeetSync

---
+++ 409-longest-palindrome/longest-palindrome.py
@@ -0,0 +1,18 @@
+class Solution:
+    def longestPalindrome(self, s: str) -> int:
+        counter = {}
+        res = 0
+
+        for c in s:
+            counter[c] = 1 + counter.get(c,0)
+            if counter[c] % 2 == 0:
+                res += 2
+
+        for c in counter:
+            if counter[c] % 2 == 1:
+                res += 1
+                break
+
+        return res
+
+        
