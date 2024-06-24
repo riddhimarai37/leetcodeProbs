7f891c503744745782ee80273ec43048021839fe
riddhimarai37 <57564654+riddhimarai37@users.noreply.github.com>
2024-06-24 16:17:42 -0400

    Time: 50 ms (44.14%) | Memory: 19.1 MB (95.21%) - LeetSync

--- 543-diameter-of-binary-tree/diameter-of-binary-tree.py
+++ 543-diameter-of-binary-tree/diameter-of-binary-tree.py
@@ -6,59 +6,20 @@
 #         self.right = right
 class Solution:
     def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
-        if not root:
-            return 0
+        res = 0
 
-        self.max_length = 0
-        def traverse(node):
-            if not node:
+        def dfs(root):
+            nonlocal res
+            if not root:
                 return 0
 
-            left = traverse(node.left)
-            right = traverse(node.right)
-            diameter = left + right
+            left = dfs(root.left)
+            right = dfs(root.right)
 
-            self.max_length = max(self.max_length, diameter)
+            res = max(res, left + right)
+            return 1 + max(left,right)
 
-            return 1 + max(left, right)
-
-        traverse(root)
-        return self.max_length
+        dfs(root)
+        return res
 
         
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        
-        # self.max_diameter = 0
-        # def depth(node):
-        #     if not node:
-        #         return 0
-
-        #     left_length = depth(node.left)
-        #     right_length = depth(node.right)
-
-        #     diameter = left_length + right_length
-
-        #     self.max_diameter = max(self.max_diameter, diameter)
-        #     return 1 + max(left_length, right_length)
-
-        # depth(root)
-        # return self.max_diameter
