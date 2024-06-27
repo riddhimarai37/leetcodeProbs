e0849ca6b1c9f5da9a44dd391e948bb10241ba10
riddhimarai37 <57564654+riddhimarai37@users.noreply.github.com>
2024-06-27 18:52:25 -0400

    Time: 58 ms (8.41%) | Memory: 16.6 MB (43.58%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.py
+++ 13-roman-to-integer/roman-to-integer.py
@@ -1,21 +1,10 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
-        # Dictionary of roman numerals
-      roman_map = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-      # Length of the given string
-      n = len(s)
-      # This variable will store result
-      num = roman_map[s[n - 1]]
-      # Loop for each character from right to left
-      for i in range(n - 2, -1, -1):
-         # Check if the character at right of current character is bigger or smaller
-         if roman_map[s[i]] >= roman_map[s[i + 1]]:
-               num += roman_map[s[i]]
-         else:
-               num -= roman_map[s[i]]
-      return num
-            
-               
-
-
-
+        roman = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+        res = 0
+        for i in range(len(s)):
+            if i + 1 < len(s) and roman[s[i]] < roman[s[i + 1]]:
+                res -= roman[s[i]]
+            else:
+                res += roman[s[i]]
+        return res
