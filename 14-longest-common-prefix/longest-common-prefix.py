0245c8579c1531d748ebe570bebb6bd8fd017d4c
riddhimarai37 <57564654+riddhimarai37@users.noreply.github.com>
2024-07-16 19:49:13 -0400

    Time: 44 ms (17.78%) | Memory: 16.8 MB (5.10%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.py
+++ 14-longest-common-prefix/longest-common-prefix.py
@@ -1,25 +1,54 @@
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
         res = ""
-        min_length = 200
-
-        for s in strs:
-            min_length = min(min_length, len(s))
-            if len(s) == min_length:
-                res = s
-
-        for s in strs:
-            if s == res:
-                continue
-            i = 0
-            while i < min_length:
-                if s[i] != res[i]:
-                    res = res[:i]
-                    min_length = len(res)
-                    break
-                i += 1
-
+        for i in range(len(strs[0])):
+            for s in strs:
+                if i == len(s) or s[i] != strs[0][i]:
+                    return res
+            res += strs[0][i]
         return res
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        # My Solution 
+        # res = ""
+        # min_length = 200
+
+        # for s in strs:
+        #     min_length = min(min_length, len(s))
+        #     if len(s) == min_length:
+        #         res = s
+
+        # for s in strs:
+        #     if s == res:
+        #         continue
+        #     i = 0
+        #     while i < min_length:
+        #         if s[i] != res[i]:
+        #             res = res[:i]
+        #             min_length = len(res)
+        #             break
+        #         i += 1
+
+        # return res
 
         
 
