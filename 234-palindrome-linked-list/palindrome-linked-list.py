31e26c0347de10a1367670b1462f47d09d869eef
riddhimarai37 <57564654+riddhimarai37@users.noreply.github.com>
2024-07-30 22:11:03 -0400

    Time: 278 ms (85.15%) | Memory: 32 MB (82.28%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.py
+++ 234-palindrome-linked-list/palindrome-linked-list.py
@@ -5,24 +5,32 @@
 #         self.next = next
 class Solution:
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
-        # Initialize a front pointer
-        self.frontPointer = head
+        fast = head
+        slow = head
 
-        # Define Recursive Call: To check front pointer against entire stack of nodes(reversed nodes).
-        def recursiveCheck(curr: Optional[ListNode]) -> bool:
-            if curr:
-                # Recursive check on next node otherwise return false
-                if not recursiveCheck(curr.next):
-                    return False
-                # Ensure front pointer is equal to stack node otherwise return false
-                elif self.frontPointer.val != curr.val:
-                    return False
-                # Move front pointer
-                else:
-                    self.frontPointer = self.frontPointer.next
-            # At end of call stack return True
-            return True
-            
-        # Return Recursive Call
-        return recursiveCheck(head)
-            
+        # find middle node
+        while fast and fast.next:
+            fast = fast.next.next
+            slow = slow.next
+
+        # reverse second half of list
+        prev = None
+        while slow:
+            temp = slow.next
+            slow.next = prev
+            prev = slow
+            slow = temp
+
+        # check palindrome
+        left = head
+        right = prev
+        while right:
+            if left.val != right.val:
+                return False
+            left = left.next
+            right = right.next
+        
+        return True
+        
+
+        
