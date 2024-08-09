8e33e6ef9d039005241f1347e5832e5796485f7b
riddhimarai37 <57564654+riddhimarai37@users.noreply.github.com>
2024-08-09 18:50:17 -0400

    Time: 101 ms (93.86%) | Memory: 17.7 MB (90.82%) - LeetSync

--- 268-missing-number/missing-number.py
+++ 268-missing-number/missing-number.py
@@ -1,9 +1,43 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        missing = len(nums)
-        for i, num in enumerate(nums):
-            missing ^= i ^ num
-        return missing
+        length = len(nums)
+        total_sum = 0
+
+        for i in range(0, len(nums)+1):
+            total_sum += i
+        
+        for n in nums:
+            total_sum -= n
+
+        return total_sum
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        # missing = len(nums)
+        # for i, num in enumerate(nums):
+        #     missing = missing ^ i ^ num
+        # return missing
+
+        # doesnt matter which order you XOR in as long as you XOR every value in the range 
+        # with every value in nums; whatever left is the missing number b/c all other nums
+        # will xor with themselves at some point
+        
 
         # Time O(n)
         # Space O(1)
