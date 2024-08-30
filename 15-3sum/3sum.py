1fd6e249576a0c4c53e2b062522f7b4f49a17729
riddhimarai37 <57564654+riddhimarai37@users.noreply.github.com>
2024-08-29 21:03:13 -0400

    Time: 653 ms (76.57%) | Memory: 20.6 MB (71.47%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -1,46 +1,40 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
+        nums = sorted(nums)
         res = []
-        nums.sort()
 
-        for idx, n in enumerate(nums):
-            # if positive number break the loop bc wont add up to 0
-            if n > 0:
-                break
-
-            # if duplicate value continue
-            if idx > 0 and n== nums[idx-1]:
+        for idx,num in enumerate(nums):
+            # same as previous
+            if idx > 0 and num == nums[idx-1]:
                 continue
 
-            left = idx + 1
-            right = len(nums) - 1
+            l = idx + 1
+            r = len(nums) - 1
+            
+            while l < r:
+                # current sum
+                three_sum = num + nums[l] + nums[r]
 
-            while left < right:
-                three_sum = n + nums[left] + nums[right]
-
-                if three_sum == 0:
-                    res.append([n, nums[left], nums[right]])
-                    left += 1
-                    right -= 1
-                    while nums[left] == nums[left - 1] and left < right:
-                        left += 1
-                elif three_sum > 0:
-                    right -= 1
+                if three_sum > 0:
+                    r -= 1
                 elif three_sum < 0:
-                    left += 1
+                    l += 1
+                else:
+                    res.append([num, nums[l], nums[r]])
+                    l += 1
+                    # skipping past duplpicates
+                    while nums[l] == nums[l-1] and l < r:
+                        l += 1
 
-                
         return res
 
+                
+            
+            
+            
 
 
-
-
-
-
-
-
-
+                
 
 
 
@@ -62,38 +56,37 @@ class Solution:
 
 
         
-        # ans = []
+        # res = []
         # nums.sort()
 
-        # for idx, num in enumerate(nums):
-        #     if num > 0:
+        # for idx, n in enumerate(nums):
+        #     # if positive number break the loop bc wont add up to 0
+        #     if n > 0:
         #         break
 
-        #     if idx > 0 and num == nums[idx-1]:
+        #     # if duplicate value continue
+        #     if idx > 0 and n== nums[idx-1]:
         #         continue
 
-        #     l = idx + 1
-        #     r = len(nums) - 1
+        #     left = idx + 1
+        #     right = len(nums) - 1
 
-        #     while l < r:
-        #         three_sum = num + nums[l] + nums[r]
+        #     while left < right:
+        #         three_sum = n + nums[left] + nums[right]
 
-        #         # if sum is positive
-        #         if three_sum > 0:
-        #             r -= 1
-        #         # if sum is negative
+        #         if three_sum == 0:
+        #             res.append([n, nums[left], nums[right]])
+        #             left += 1
+        #             right -= 1
+        #             # skip past duplicates
+        #             while nums[left] == nums[left - 1] and left < right:
+        #                 left += 1
+        #         elif three_sum > 0:
+        #             right -= 1
         #         elif three_sum < 0:
-        #             l += 1
-        #         # if sum == 0
-        #         else:
-        #             ans.append([num, nums[l], nums[r]])
-        #             l += 1
-        #             r -= 1
-        #             while nums[l] == nums[l-1] and l < r:
-        #                 l += 1
-
-        # return ans
-
+        #             left += 1
+                
+        # return res
                 
     # Time: O(n^2)
     # Space: O(1)
