ebc6bca1495b81b6535ec5fb1f6059344a71f5c7
riddhimarai37 <57564654+riddhimarai37@users.noreply.github.com>
2024-10-16 19:03:51 -0400

    Time: 91 ms (43.85%) | Memory: 22 MB (26.21%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -1,22 +1,69 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        count_dict = {}
+        counts = {}
+        freq = [[] for i in range(len(nums) + 1)]
         res = []
-        count = [[] for i in range(len(nums) + 1)]
 
         for n in nums:
-            count_dict[n] = 1 + count_dict.get(n,0)
+            counts[n] = 1 + counts.get(n,0)
 
-        for n,c in count_dict.items():
-            count[c].append(n)
+        for n,c in counts.items():
+            freq[c].append(n)
 
-        for i in range(len(count)-1,0,-1):
-            for n in count[i]:
-                res.append(n)
-                if len(res) == k:
-                    return res
+        idx = len(freq) - 1
+
+        while len(res) < k:
+            if len(freq[idx]) > 0:
+                for n in freq[idx]:
+                    res.append(n)
+            idx -= 1
 
         return res
+
+        
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+        # count_dict = {}
+        # res = []
+        # count = [[] for i in range(len(nums) + 1)]
+
+        # for n in nums:
+        #     count_dict[n] = 1 + count_dict.get(n,0)
+
+        # for n,c in count_dict.items():
+        #     count[c].append(n)
+
+        # for i in range(len(count)-1,0,-1):
+        #     for n in count[i]:
+        #         res.append(n)
+        #         if len(res) == k:
+        #             return res
+
+        # return res
+
+        # Space: O(n) Time: O(n)
             
             
 
