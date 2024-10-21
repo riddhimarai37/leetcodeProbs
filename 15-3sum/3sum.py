c23201f2f844df58c85475d9aa50657c4ab3a066
riddhimarai37 <57564654+riddhimarai37@users.noreply.github.com>
2024-10-20 22:58:58 -0400

    Time: 433 ms (99.16%) | Memory: 19.7 MB (98.53%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -3,29 +3,32 @@ class Solution:
         nums = sorted(nums)
         res = []
 
-        for idx,n in enumerate(nums):
-            if idx > 0 and nums[idx] == nums[idx-1]:
-                continue
+        for i,n in enumerate(nums):
             if n > 0:
                 continue
+            
+            if i > 0 and nums[i-1] == nums[i]:
+                continue
 
-            l = idx + 1
+            l = i+1
             r = len(nums) - 1
 
             while l < r:
-                threeSum = n + nums[l] + nums[r]
+                curr_sum = n + nums[l] + nums[r]
 
-                if threeSum > 0:
+                if curr_sum > 0:
                     r -= 1
-                elif threeSum < 0:
+                elif curr_sum < 0:
                     l += 1
                 else:
                     res.append([n, nums[l], nums[r]])
+                    r -= 1
                     l += 1
-                    while l < r and nums[l] == nums[l-1]:
+                    while l < r and nums[l-1] == nums[l]:
                         l += 1
 
         return res
+
 
             
 
