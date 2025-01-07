1074255e27bcb0816d16e28c2d8cc24fafef889d
riddhimarai37 <57564654+riddhimarai37@users.noreply.github.com>
2025-01-06 22:30:15 -0500

    Time: 7 ms (81.22%) | Memory: 18.2 MB (32.27%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -5,15 +5,16 @@ class Solution:
         r = len(s) - 1
 
         while l < r: 
-            if s[l].isalnum() and s[r].isalnum() and s[l] != s[r]:
+            while l < r and not s[l].isalnum():
+                l += 1
+            while r > l and not s[r].isalnum():
+                r -= 1
+
+            if s[l] != s[r]:
                 return False
-            elif not s[l].isalnum():
-                l += 1
-            elif not s[r].isalnum():
-                r -= 1
-            else: 
-                l += 1
-                r -= 1
+
+            l += 1
+            r-=1 
 
         return True  
             
