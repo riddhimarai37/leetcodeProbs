afce6158db0314e07ea1304960721ad0650db0a9
riddhimarai37 <57564654+riddhimarai37@users.noreply.github.com>
2025-01-21 21:52:54 -0500

    Time: 58 ms (89.76%) | Memory: 17.9 MB (66.72%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.py
+++ 76-minimum-window-substring/minimum-window-substring.py
@@ -3,29 +3,86 @@ class Solution:
         if t == "":
             return ""
 
-        countT, window = {}, {}
-        for c in t:
-            countT[c] = 1 + countT.get(c, 0)
+        # initialize t count
+        t_count = {}
+        window = {}
+        for c in t: 
+            t_count[c] = 1 + t_count.get(c,0)
 
-        have, need = 0, len(countT)
-        res, resLen = [-1, -1], float("infinity")
+        have = 0
+        need = len(t_count)
+        
+        res = [-1,-1]
+        resLen = float("infinity")
         l = 0
+
         for r in range(len(s)):
             c = s[r]
-            window[c] = 1 + window.get(c, 0)
+            window[c] = 1 + window.get(c,0)
 
-            if c in countT and window[c] == countT[c]:
+            if c in t_count and window[c] == t_count[c]:
                 have += 1
 
             while have == need:
-                # update our result
-                if (r - l + 1) < resLen:
-                    res = [l, r]
-                    resLen = r - l + 1
-                # pop from the left of our window
+                # update result 
+                if (r-l+1) < resLen:
+                    res = [l,r]
+                    resLen = (r-l+1)
+                # pop from left of window
                 window[s[l]] -= 1
-                if s[l] in countT and window[s[l]] < countT[s[l]]:
+                if s[l] in t_count and window[s[l]] < t_count[s[l]]:
                     have -= 1
                 l += 1
-        l, r = res
-        return s[l : r + 1] if resLen != float("infinity") else ""
+        l,r = res
+
+        if resLen == float('infinity'):
+            return ""
+
+        return s[l:r+1]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+        # if t == "":
+        #     return ""
+
+        # countT, window = {}, {}
+        # for c in t:
+        #     countT[c] = 1 + countT.get(c, 0)
+
+        # have, need = 0, len(countT)
+        # res, resLen = [-1, -1], float("infinity")
+        # l = 0
+        # for r in range(len(s)):
+        #     c = s[r]
+        #     window[c] = 1 + window.get(c, 0)
+
+        #     if c in countT and window[c] == countT[c]:
+        #         have += 1
+
+        #     while have == need:
+        #         # update our result
+        #         if (r - l + 1) < resLen:
+        #             res = [l, r]
+        #             resLen = r - l + 1
+        #         # pop from the left of our window
+        #         window[s[l]] -= 1
+        #         if s[l] in countT and window[s[l]] < countT[s[l]]:
+        #             have -= 1
+        #         l += 1
+        # l, r = res
+        # return s[l : r + 1] if resLen != float("infinity") else ""
