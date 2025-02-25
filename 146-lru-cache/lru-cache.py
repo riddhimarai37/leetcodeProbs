fb344c9c461a4844b6fc512c61b2febbd03c5613
riddhimarai37 <57564654+riddhimarai37@users.noreply.github.com>
2025-02-24 20:00:42 -0500

    Time: 187 ms (15.27%) | Memory: 78.6 MB (46.34%) - LeetSync

--- 146-lru-cache/lru-cache.py
+++ 146-lru-cache/lru-cache.py
@@ -1,53 +1,64 @@
 class ListNode:
-    def __init__(self, key, val):
+    def __init__(self, key, value):
         self.key = key
-        self.val = val
+        self.value = value
         self.next = None
         self.prev = None
 
 class LRUCache:
+
     def __init__(self, capacity: int):
-        self.capacity = capacity
-        self.dic = {}
-        self.head = ListNode(-1, -1)
-        self.tail = ListNode(-1, -1)
+        self.cap = capacity
+        self.pairs = {}
+        self.head = ListNode(-1,-1)
+        self.tail = ListNode(-1,-1)
         self.head.next = self.tail
         self.tail.prev = self.head
-
+    
     def get(self, key: int) -> int:
-        if key not in self.dic:
+        if key not in self.pairs: 
             return -1
-        
-        node = self.dic[key]
+
+        node = self.pairs[key]
         self.remove(node)
-        self.add(node)
-        return node.val
-        
+        self.insert(node)
+
+        return node.value
+    
     def put(self, key: int, value: int) -> None:
-        if key in self.dic:
-            old_node = self.dic[key]
+        if key in self.pairs:
+            old_node = self.pairs[key]
             self.remove(old_node)
         
-        node = ListNode(key, value)
-        self.dic[key] = node
-        self.add(node)
-        
-        if len(self.dic) > self.capacity:
-            node_to_delete = self.head.next
-            self.remove(node_to_delete)
-            del self.dic[node_to_delete.key]
+        new_node = ListNode(key,value)
+        self.pairs[key] = new_node
+        self.insert(new_node)
 
-    def add(self, node):
-        previous_end = self.tail.prev
-        previous_end.next = node
-        node.prev = previous_end
+        if len(self.pairs) > self.cap:
+            lru_node = self.head.next
+            self.remove(lru_node)
+            del self.pairs[lru_node.key]
+        
+    def insert(self, node):
+        curr_tail = self.tail.prev
+        curr_tail.next= node
+        node.prev = curr_tail
         node.next = self.tail
         self.tail.prev = node
-        
+
     def remove(self, node):
         node.prev.next = node.next
         node.next.prev = node.prev
+
+
         
+
+        
+
+
+        
+
+
 # Your LRUCache object will be instantiated and called as such:
 # obj = LRUCache(capacity)
 # param_1 = obj.get(key)
