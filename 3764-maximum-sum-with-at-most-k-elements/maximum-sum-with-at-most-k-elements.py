cfff4a0bb2177782f45bac9379983d6dc3965b20
riddhimarai37 <57564654+riddhimarai37@users.noreply.github.com>
2025-03-02 17:37:02 -0500

    Time: 165 ms (64.61%) | Memory: 46.9 MB (58.03%) - LeetSync

--- 3764-maximum-sum-with-at-most-k-elements/maximum-sum-with-at-most-k-elements.py
+++ 3764-maximum-sum-with-at-most-k-elements/maximum-sum-with-at-most-k-elements.py
@@ -1,41 +1,25 @@
 class Solution:
     def maxSum(self, grid: List[List[int]], limits: List[int], k: int) -> int:
-        n = len(grid)
-        if k == 0:
-            return 0
+        a = []
         
-        # max heap 
-        max_heap = []
-
-        for i in range(n):
-            size = limits[i]
-            # min heap
-            min_heap = []
+        # For each row in the grid
+        for i in range(len(grid)):
+            # Sort the row in descending order
+            s = sorted(grid[i], reverse=True)
             
-            if size != 0:
-                for num in grid[i]:
-                    if len(min_heap) < size:
-                        heapq.heappush(min_heap, num)
-                    else:
-                        if min_heap[0] < num:
-                            heapq.heappop(min_heap)
-                            heapq.heappush(min_heap, num)
-
-            while min_heap:
-              val = heapq.heappop(min_heap)
-              heapq.heappush(max_heap, -val)
-
-        max_sum = 0
-        cnt = 0
-        while max_heap:
-            val = -heapq.heappop(max_heap)
-            max_sum += val
-            cnt += 1
-
-            if cnt == k:
-                break
-
-        return max_sum
+            # Select top elements based on limits
+            a.extend(s[:limits[i]])
+        
+        # Sort all selected elements in ascending order
+        a.sort()
+        
+        s = 0
+        
+        # Pick the largest k elements and sum them up
+        for i in range(k):
+            s += a.pop()
+        
+        return s
 
         
         
